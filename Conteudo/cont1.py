23bb8ecb73af15593b0705f058be5b96cdc52653
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-03-05 11:43:55 -0300

    Create cont1.py

---
+++ Conteudo/cont1.py
@@ -0,0 +1,5 @@
+# int: para números inteiros, exemplo:"7, 0, -4".
+# bool: para valores lógicos, como:"true/false".
+# float: números reais com virgulas, exemplo:" 0.45, 7.6".
+# str: para strings que são palavras, carácteres ou textos, exemplo:"ola, 7.5".
+# Para saber o tipo de váriavel, utiliza-se o comando:" print(type(variavel))"
