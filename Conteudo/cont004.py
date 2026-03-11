594db1b6d5bc943fe8757f335e456626888ceee1
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-03-11 11:38:45 -0300

    Create cont004.py

---
+++ Conteudo/cont004.py
@@ -0,0 +1,7 @@
+# O "+" representa uma operação de adição.
+# O "-" representa uma operação de subtração.
+# O "*" utilizado para operações de multiplicação.
+# O "/" é utilizada para uma operação de divisão.
+# O "**" representa uma operação de exponenciação.
+# O "//" é utilizado para fazer a divisão inteira.
+# O "%" é utilizado para o resto da divisão.
