ba80f6416c11a7b01b61e2a789dcef37a331b8a4
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-03-16 10:54:13 -0300

    Create cont5.py

---
+++ Conteudo/cont5.py
@@ -0,0 +1,16 @@
+#                              ORDEM DE PRECEDÊNCIA:
+# 1ª: expressão dentro de parenteses.
+# 2ª: em seguida vem as potenciações.
+# 3ª: em terceiro lugar a multiplicação, a divisão normal, a divisão inteira e o resto da divisão.
+# 4ª: em quarto, soma e subração
+#                                     EXEMPLO:
+# '5+3*2==' 
+# '3*5+4**2='
+# '3*(5+4)**2='
+# - O python é utilizado em cálculos científicos é o tamanho da memória ou seja conseguimos chgar a números extremamente grandes.
+#                                OUTROS OPERADORES:
+# - Para exponenciação podemos usar o pow(x,y) onde o número X é elevado ao número Y.
+# - Para raiz quadrada podemos usar o **(½), elevando o número a ½ nos obtemos a raiz quadrada, para raiz cúbica **(1/3), e assim por diante.
+# - Podemos usar os operadores em strings.
+# - Ex '='*5 vai ficar '====='
+#
