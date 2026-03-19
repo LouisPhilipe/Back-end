a4c8390d580dcb6d76725c7f49490dec018c54ab
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-03-19 08:07:35 -0300

    Create ex8.py

---
+++ exercicios/ex8.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite o valor das notas de Lingua portuguesa de seu aluno:"))
+n2 = int(input("Digite o valor das notas de Matemática de seu aluno:"))
+print("A media das notas do seu aluno é: {}"
+.format ((n1+n2)/2))
