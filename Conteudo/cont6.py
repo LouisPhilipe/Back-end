c62e3f834960b1faab90b8d4fa7a087ceaf67efd
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-03-26 08:53:02 -0300

    Update cont6.py

--- Conteudo/cont6.py
+++ Conteudo/cont6.py
@@ -3,3 +3,11 @@ num = int(input('Digite um número: ' ))
 raiz = math.sqrt(num)
 print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz))) #arrendonda para cima
 print('A raiz de {} é igual a {}'.format(num, math.floor(raiz))) #arredonda para baixo
+
+
+
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} arredondada para maior é igual a {}'.format(num, ceil(raiz))) #arredonda pra cima
+print('a raiz de {} arredondada para menor é igual a {}'.format(num, floor(raiz))) #arredonda para baixo 
