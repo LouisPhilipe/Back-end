86b4183a3d642fd07e7c284173af85113cd908d4
LouisPhilipe <louis.paula@escola.pr.gov.br>
2026-05-12 09:41:08 -0300

    Update ex28.py

--- exercicios/ex28.py
+++ exercicios/ex28.py
@@ -1,8 +1,8 @@
 import random
-num = random.randint(1,5)
+num = random.randint(0,5)
 res = int(input("Digite um número entre 0 a 5: "))
 if res == num:
     print("Você acertou o número sorteado!")
 else:
     print("Você errou o número sorteado, tente novamente!")
-    print("O número sorteado era: {}" .format(num))
+    print("O número sorteado era: {}, e não: {}" .format(num,res))
